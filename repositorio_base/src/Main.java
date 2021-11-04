8117116c271e24a9e0799f63f2e308b68cc6f34e
anna sganderla <sganderlakarlaanna@gmail.com>
2021-11-04 15:50:39 -0300

    add alterações

--- repositorio_base/src/Main.java
+++ repositorio_base/src/Main.java
@@ -2,6 +2,7 @@ import Controller.CinemaController;
 import Model.Cinema;
 import Model.Sessao;
 import View.CinemaView;
+import View.SessaoView;
 
 import java.io.IOException;
 
@@ -10,9 +11,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         CinemaView cinemaView = new CinemaView();
+        SessaoView sessaoView = new SessaoView();
 
         cinemaView.carregarCinemas();
         cinemaView.menuCinema();
+
+//        sessaoView.cadastrarSessao(Long.valueOf(01));
+//        sessaoView.listasSessoesArq();
 
     }
 
